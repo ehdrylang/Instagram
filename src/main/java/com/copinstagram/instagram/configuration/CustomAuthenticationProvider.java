7dc9e329025f51b34bfdbdf279331319c63bc125
Dong gyo Jeong <ehdrylang@naver.com>
2020-03-12 23:55:06 +0900

    customAuthenticationProvider 구현

--- src/main/java/com/copinstagram/instagram/configuration/CustomAuthenticationProvider.java
+++ src/main/java/com/copinstagram/instagram/configuration/CustomAuthenticationProvider.java
@@ -2,15 +2,12 @@ package com.copinstagram.instagram.configuration;
 
 import com.copinstagram.instagram.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
@@ -23,15 +20,40 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
     @Override
     @Transactional
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        if(authentication == null){
+            throw new InternalAuthenticationServiceException("Authentication is null");
+        }
         String username = authentication.getName();
+        if(authentication.getCredentials() == null){
+            throw new AuthenticationCredentialsNotFoundException("Credentials is null");
+        }
         String password = authentication.getCredentials().toString();
-        String encodedPassword = passwordEncoder.encode(password);
         UserDetails loadedUser = memberService.loadUserByUsername(username);
         if(loadedUser == null){
             throw new InternalAuthenticationServiceException("UserDetailsService returned null, which is an interface contract violation");
         }
-
-        return new UsernamePasswordAuthenticationToken(loadedUser, null, loadedUser.getAuthorities());
+        /* checker*/
+        if(!loadedUser.isAccountNonLocked()){
+            throw new LockedException("User account is locked");
+        }
+        if(!loadedUser.isEnabled()){
+            throw new DisabledException("User is disabled");
+        }
+        if(!loadedUser.isAccountNonExpired()){
+            throw new AccountExpiredException("User account has expired");
+        }
+        /* 실질적인 인증 */
+        if(!passwordEncoder.matches(password, loadedUser.getPassword())){
+            throw new BadCredentialsException("Password does not match stored value");
+        }
+        /* checker */
+        if(!loadedUser.isCredentialsNonExpired()){
+            throw new CredentialsExpiredException("User credentials have expired");
+        }
+        /* 인증 완료 */
+        UsernamePasswordAuthenticationToken result = new UsernamePasswordAuthenticationToken(loadedUser, null, loadedUser.getAuthorities());
+        result.setDetails(authentication.getDetails());
+        return result;
     }
 
     @Override
